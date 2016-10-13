9a29738e412c077afa172d1ad48ea93e3c5bcfd6
Fred <fwang@campuscruiser.com>
2016-10-13 13:49:55 +0800

    init version

---
+++ src/components/GalleryByReactApp.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var React = require('react/addons');
+var ReactTransitionGroup = React.addons.TransitionGroup;
+
+// CSS
+require('normalize.css');
+require('../styles/main.css');
+
+var imageURL = require('../images/yeoman.png');
+
+var GalleryByReactApp = React.createClass({
+  render: function() {
+    return (
+      <div className="main">
+        <ReactTransitionGroup transitionName="fade">
+          <img src={imageURL} />
+             <span>hello!!!!!!</span>
+        </ReactTransitionGroup>
+      </div>
+    );
+  }
+});
+React.render(<GalleryByReactApp />, document.getElementById('content')); // jshint ignore:line
+
+module.exports = GalleryByReactApp;
